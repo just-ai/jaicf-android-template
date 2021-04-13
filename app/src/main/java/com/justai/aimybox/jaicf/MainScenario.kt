ecfef9eaee11351ed31383219d20beda622b4010
bgubanov <b.gubanov@list.ru>
2021-04-13 15:10:23 +0300

    Changed implementation of Scenario interface to creation Scenario by builder

--- app/src/main/java/com/justai/aimybox/jaicf/MainScenario.kt
+++ app/src/main/java/com/justai/aimybox/jaicf/MainScenario.kt
@@ -6,75 +6,70 @@ import com.justai.jaicf.activator.rasa.rasa
 import com.justai.jaicf.builder.Scenario
 import com.justai.jaicf.channel.aimybox.aimybox
 import com.justai.jaicf.hook.AfterProcessHook
-import com.justai.jaicf.model.scenario.Scenario
-import com.justai.jaicf.model.scenario.getValue
 
-object MainScenario: Scenario {
+val MainScenario = Scenario {
+    /**
+     * Appends these buttons for every response
+     */
+    handle<AfterProcessHook> {
+        reactions.buttons(
+            "Help me!", "How are you?", "What is your name?"
+        )
+    }
 
-    override val model by Scenario {
-        /**
-         * Appends these buttons for every response
-         */
-        handle<AfterProcessHook> {
-            reactions.buttons(
-                "Help me!", "How are you?", "What is your name?"
-            )
+    state("hello") {
+        activators {
+            intent("hello")
+            intent("greet")
         }
 
-        state("hello") {
-            activators {
-                intent("hello")
-                intent("greet")
-            }
-
-            action {
-                reactions.run {
-                    image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
-                    sayRandom("Hello!", "Hello there!")
-                }
+        action {
+            reactions.run {
+                image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
+                sayRandom("Hello!", "Hello there!")
             }
         }
+    }
 
-        state("bye") {
-            activators {
-                intent("bye")
-                intent("goodbye")
-            }
-
-            action {
-                reactions.run {
-                    image("https://media.giphy.com/media/EE185t7OeMbTy/source.gif")
-                    say("Bye bye!")
-                    aimybox?.endConversation()
-                }
-            }
+    state("bye") {
+        activators {
+            intent("bye")
+            intent("goodbye")
         }
 
-        state("smalltalk", noContext = true) {
-            activators {
-                anyIntent()
-            }
-
-            action {
-                activator.caila?.topIntent?.answer?.let {
-                    reactions.say(it)
-                }
-
-                activator.rasa?.run {
-                    reactions.say("Hmm... I don't know what to say.")
-                }
-
-                activator.dialogflow?.run {
-                    reactions.say(queryResult.fulfillmentText)
-                }
+        action {
+            reactions.run {
+                image("https://media.giphy.com/media/EE185t7OeMbTy/source.gif")
+                say("Bye bye!")
+                aimybox?.endConversation()
             }
         }
+    }
 
-        fallback {
-            reactions.sayRandom(
-                "Sorry, I didn't get that.",
-                "Sorry, could you repeat please?"
-            )
+    state("smalltalk", noContext = true) {
+        activators {
+            anyIntent()
         }
+
+        action {
+            activator.caila?.topIntent?.answer?.let {
+                reactions.say(it)
+            }
+
+            activator.rasa?.run {
+                reactions.say("Hmm... I don't know what to say.")
+            }
+
+            activator.dialogflow?.run {
+                reactions.say(queryResult.fulfillmentText)
+            }
+        }
+    }
+
+    fallback {
+        reactions.sayRandom(
+            "Sorry, I didn't get that.",
+            "Sorry, could you repeat please?"
+        )
     }
 }
